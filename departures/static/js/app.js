951269c2498d698be959c7b303d6040fa29d3962
Shashi Kumar P <shashiessp@gmail.com>
2020-03-17 21:37:32 -0500

    update current date and time

--- departures/static/js/app.js
+++ departures/static/js/app.js
@@ -11,25 +11,57 @@ app.config(function($interpolateProvider, $httpProvider, RestangularProvider){
 
 app.controller('ScheduleCtrl', function($scope, Restangular) {
 
+    $scope.getCurrentTime = function() {
+        let today     = new Date();
+        let hours    = today.getHours();
+        let minutes  = today.getMinutes();
 
-  $scope.query = 'place-north'; //inital value
+        let ampm = hours >= 12 ? 'pm' : 'am';
 
-  $scope.current_date = new Date();
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        let currentTime = hours + ':' + minutes + ' ' + ampm;
+        return currentTime;
+    }
 
-  $scope.current_time = new Date()
 
-  $scope.loadData = function(){
-    var url = 'api/?station='+$scope.query;
-    var resource = Restangular.all(url);
+    $scope.query = 'place-north'; //inital value
+
+    let today = new Date();
+    let dd = today.getDate();
+
+    let mm = today.getMonth()+1; 
+    let yyyy = today.getFullYear();
+    
+    if(dd<10) 
+        {
+            dd='0'+dd;
+        } 
+
+    if(mm<10) 
+    {
+        mm='0'+mm;
+    }
+
+    $scope.current_date = mm+'-'+dd+'-'+yyyy;
+    $scope.current_day_name = today.toString().split(' ')[0];
+
+    $scope.current_time = $scope.getCurrentTime();
+
+    $scope.loadData = function(){
+        let url = 'api/?station='+$scope.query;
+        let resource = Restangular.all(url);
         
-    resource.getList().then(function(data){
-        $scope.results = data;
-    });
-  }
+        resource.getList().then(function(data){
+            $scope.results = data;
+        });
+    }
 
-  setInterval(function(){
+      setInterval(function(){
+        $scope.loadData();
+        $scope.current_time = $scope.getCurrentTime();
+      }, 30000)
+
     $scope.loadData();
-  }, 30000)
-
-  $scope.loadData();
 });
